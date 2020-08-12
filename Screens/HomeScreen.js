2c2f834c0e8a5e00e5a772aa6da012fa5fab1041
Workgeek100 <maybha_70@hotmail.com>
2020-08-12 20:12:47 +0300

    P58

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import {Text, Image, View, TouchableOpacity, StyleSheet} from 'react-native';
+import AppHeader from '../components/Appheader';
+import db from '../config';
+
+export default class Homescreen extends React.Component{
+
+likePressed(){
+  var like=db.ref('Rating/'+'/')
+  like.update({
+    'likePressed':1
+  })
+}
+
+dislikePressed(){
+  var dislike = db.ref('Rating/'+'/')
+  dislike.update({
+    'dislikePressed':1
+  })
+}
+
+  render(){
+    return(
+      <View>
+      <TouchableOpacity 
+      style={styles.jokebutton}
+      onPress={()=>{
+        this.props.navigation.navigate('Jokescreen')
+      }}>
+      <Text style={{fontSize:20}}>Read A Joke</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+      style={styles.horoscope}
+      onPress={()=>{
+        this.props.navigation.navigate('Horoscope')
+      }}>
+      <Text style={{fontSize:20}}>Horoscope</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+      style={styles.weather}
+      onPress={()=>{
+        this.props.navigation.navigate('Weather')
+      }}>
+      <Text style={{fontSize:20}}>Weather</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+      style={styles.topnews}
+      onPress={()=>{
+        this.props.navigation.navigate('Topnews')
+      }}>
+      <Text style={{fontSize:20}}>Top News</Text>
+      </TouchableOpacity>
+      <Text style={{ textAlign:'center',margin:5, fontSize:30, color:"blue"}}>Rate Us</Text>
+      <TouchableOpacity
+      onPress={()=>{
+        this.likePressed()
+      }}>
+      <Image
+      style={{width:50, height:50,marginLeft:70}}
+      source={require('../assets/thumbsup.png')}
+      />
+      </TouchableOpacity>
+      <TouchableOpacity 
+      onPress={()=>{
+        this.dislikePressed()
+      }}>
+      <Image
+      style={{width:50, height:50,marginLeft:200, marginTop:-35}}
+      source={require('../assets/thumbsdown.png')}
+      />
+      </TouchableOpacity>
+      <AppHeader />
+      </View>
+    )
+  }
+}  
+
+const styles = StyleSheet.create({
+  jokebutton:{
+    marginTop:90,
+    marginLeft:70,
+    backgroundColor:"yellow",
+    borderWidth:5,
+    borderRadius:100,
+    width:200,
+    height:50,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+
+  horoscope:{
+    marginTop:20,
+    marginLeft:70,
+    backgroundColor:"green",
+    borderRadius:100,
+    borderWidth:5,
+    alignItems:'center',
+    justifyContent:'center',
+    width:200,
+    height:50
+  },
+
+  weather:{
+    marginTop:20,
+    marginLeft:70,
+    backgroundColor:"lightblue",
+    borderRadius:100,
+    borderWidth:5,
+    alignItems:'center',
+    justifyContent:'center',
+    width:200,
+    height:50
+  },
+
+
+  topnews:{
+    marginTop:20,
+    marginLeft:70,
+    backgroundColor:"purple",
+    borderRadius:100,
+    borderWidth:5,
+    alignItems:'center',
+    justifyContent:'center',
+    width:200,
+    height:50
+  }
+});
+
